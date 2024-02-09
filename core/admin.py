ff1deceb0e9c615725ea57b10ef1df7a0653565a
Cristian <cronodark5@gmail.com>
2024-02-08 20:08:52 -0600

    agregando los modelos al admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Country
-admin.site.register(Country)
+from .models import Country, Nationality, Position, MediaChannel, NationalAcreditation, InternationalAccreditation, SecurityAccreditation, FlightRequest, Vehicle, VehicleAccreditation, CommunicationEquipmentDeclaration, EquipmentItem, CommunicationEquipmentDeclarationEquipmentItem, GeneralVehicleAccreditation
+
+modelos = [Country, Nationality, Position, MediaChannel, NationalAcreditation, InternationalAccreditation, SecurityAccreditation, FlightRequest, Vehicle, VehicleAccreditation, CommunicationEquipmentDeclaration, EquipmentItem, CommunicationEquipmentDeclarationEquipmentItem, GeneralVehicleAccreditation]
+
+for modelo in modelos:
+    admin.site.register(modelo)
+
+
 
 # Register your models here.
