36c24f7398c822bb9e19c3c5bcab8b1a940698b4
Jason Escobar <jasonescobar20@outlook.com>
2024-02-10 20:58:48 -0600

    feat: customize admin and add many to many fields to international accreditation form

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,113 @@
 from django.contrib import admin
-from .models import Country, Nationality, Position, MediaChannel, NationalAcreditation, InternationalAccreditation, SecurityAccreditation, FlightRequest, Vehicle, VehicleAccreditation, CommunicationEquipmentDeclaration, EquipmentItem, CommunicationEquipmentDeclarationEquipmentItem, GeneralVehicleAccreditation
+from django.db import models
+from django import forms
+from django.utils.translation import gettext as _
 
-modelos = [Country, Nationality, Position, MediaChannel, NationalAcreditation, InternationalAccreditation, SecurityAccreditation, FlightRequest, Vehicle, VehicleAccreditation, CommunicationEquipmentDeclaration, EquipmentItem, CommunicationEquipmentDeclarationEquipmentItem, GeneralVehicleAccreditation]
+from core.models import Country, Nationality, Position, MediaChannel, NationalAcreditation, SecurityAccreditation, FlightRequest, Vehicle, VehicleAccreditation, CommunicationEquipmentDeclaration, EquipmentItem, CommunicationEquipmentDeclarationEquipmentItem, GeneralVehicleAccreditation
+
+from core.modelos.international_accreditation import InternationalAccreditation, MedicalHistory, Allergy, Inmunization
+
+modelos = [Country, Nationality, Position, MediaChannel, NationalAcreditation, SecurityAccreditation, FlightRequest, Vehicle, VehicleAccreditation, CommunicationEquipmentDeclaration, EquipmentItem, CommunicationEquipmentDeclarationEquipmentItem, GeneralVehicleAccreditation]
 
 for modelo in modelos:
     admin.site.register(modelo)
 
 
+@admin.register(InternationalAccreditation)
+class InternationalAccreditationAdmin(admin.ModelAdmin):
+    
+    fieldsets = [
+        (_('Personal Info'), {
+            'fields': [
+                'first_name',
+                'last_name',
+                'image',
+                'nationality',
+                'passport_id',
+                'position',
+                'media_channel',
+                'institution',
+                'address',
+                'phone',
+                'cellphone',
+                'email',
+                'birthday',
+                'birthplace',
+                'authorized_by',
+                'date',
+                'letter_of_authorization',
+                'accreditation_type',
+            ]
+        }),
+        
+        (_('Medical Data'), {
+            'fields': [
+                'blood_type',
+                'age',
+                'diseases_under_treatment',
+                'medications_in_use',
+                'have_allergies',
+                'allergies',
+                'has_inmunizations',
+                'inmunizations',
+                'have_medical_history',
+                'medical_histories',
+                'surgical_history',
+                'has_personal_doctor',
+                'doctor_name',
+            ]
+        }),
+        
+        (_('Accomodation Info'), {
+            'fields': [
+                'hotel_name',
+                'hotel_address',
+                'hotel_phone',
+            ]
+        }),
+        
+        (_('Flights Information'), {
+            'fields': [
+                'flight_arrival_date',
+                'flight_arrival_time',
+                'flight_arrival_number',
+                'fligth_procedence',
+                'flight_departure_date',
+                'flight_departure_time',
+                'flight_departure_number',
+                'flight_destination',
+            ]
+        })
+    ]
+    
+    def render_change_form(self, request, context, *args, **kwargs):
+        
+        context['adminform'].form.fields['medical_histories'].widget = forms.CheckboxSelectMultiple(
+            choices=[
+                (item.id, item.name)
+                for item in MedicalHistory.objects.all()
+            ]
+        )
+        
+        context['adminform'].form.fields['allergies'].widget = forms.CheckboxSelectMultiple(
+            choices=[
+                (item.id, item.name)
+                for item in Allergy.objects.all()
+            ]
+        )
+        
+        context['adminform'].form.fields['inmunizations'].widget = forms.CheckboxSelectMultiple(
+            choices=[
+                (item.id, item.name)
+                for item in Inmunization.objects.all()
+            ]
+        )
+        return super().render_change_form(request, context, *args, **kwargs)
+    
 
-# Register your models here.
+@admin.register(Allergy)
+class AllergyAdmin(admin.ModelAdmin): ...
+
+
+@admin.register(Inmunization)
+class InmunizationAdmin(admin.ModelAdmin): ...
--- core/models.py
+++ core/models.py
@@ -11,6 +11,7 @@ def create_image_path(instance, filename: str):
 class Country(models.Model):
     name = models.CharField(max_length=150)
 
+
 class Nationality(models.Model):
     name = models.CharField(max_length=150)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
@@ -60,94 +61,6 @@ class NationalAcreditation(models.Model):
     accreditation_type = models.CharField(max_length=120, choices=AccreditationType.choices)
     authorized_by = models.CharField(max_length=150, null=True)
     date = models.DateField()
-
-
-class InternationalAccreditation(models.Model):
-    
-    def upload_file_name(self, filename):
-        return f'international_accreditation/{self.accreditation_type}/{filename}'
-    
-    class Allergies(models.TextChoices):
-        ENVIRONMENTAL = 'Ambientales', _('Ambientales')
-        NUTRITIONAL = 'Alimenticias', _('Alimenticias')
-        ANALGESICS = 'Analgesicos', _('Analgesicos')
-        ANTIBIOTICS = 'Antibioticos', _('Antibioticos')
-        
-        
-    class Inmunizations(models.TextChoices):
-        CONTRAST_CHANNELS = 'Medios de contraste', _('Medios de contraste')
-        TETANO = 'Tetano', _('Tetano')
-        HEPATITIS = 'Hepatitis', _('Hepatitis')
-        OTHERS = 'Otros', _('Otros')
-        
-    class MEDICAL_HISTORIES(models.TextChoices):
-        ISCHEMIC_HEART_DISEASE = 'Cardiopatia Isquemica', _('Cardiopatia Isquemica')
-        MYOCARDIAL_INFARCTION = 'Infarto del miocardio', _('Infarto del miocardio')
-        ULTIMATE_PACEMAKER = 'Marcapaso definitivo', _('Marcapaso definitivo')
-        ANGINA = 'Angina', _('Angina')
-        DIABETES_MELLITUS = 'Diabetes Mellitus', _('Diabetes Mellitus')
-        ARTERIAL_HYPERTENSION = 'Hipertension arterial', _('Hipertension arterial')
-        ARRHYTHMIA = 'Arritmias', _('Arritmia')
-        OTHERS = 'Otros', _('Otros')
-        
-    class AccreditationType(models.TextChoices):
-        OFFICIAL_DELEGATION_HEAD = 'Jefe de delegacion oficial', _('jefe de delegacion oficial')
-        OFFICIAL_DELEGATION = 'Delegacion Oficial', _('Delegacion Oficial')
-        PROTOCOL = 'Protocolo', _('Protocolo')
-        SECURITY = 'Seguridad', _('Seguridad')
-        SUPPORT_STAFF = 'Personal de apoyo', _('Personal de apoyo')
-        OFFICIAL_PRESS = 'Prensa oficial', _('Prensa oficial')
-        TRIPULATION = 'Tripulacion', _('Tripulacion')
-        COMERCIAL_PRESS = 'Prensa Comercial', _('Prensa Comercial')
-    
-    country_of_origin = models.ForeignKey(Country, on_delete=models.PROTECT, related_name='international_accreditations')
-    created_at = models.DateTimeField(auto_now_add=True)
-    first_name = models.CharField(max_length=150)
-    image = models.ImageField(upload_to=create_image_path)
-    last_name = models.CharField(max_length=150)
-    nationality = models.ForeignKey(Nationality, on_delete=models.CASCADE, related_name='international_accreditations')
-    passport_id = models.CharField(max_length=100)
-    position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name='international_accreditations')
-    letter_of_authorization = models.FileField(upload_to=create_image_path)
-    media_channel = models.ForeignKey(MediaChannel, on_delete=models.CASCADE, related_name='international_accreditations')
-    institution = models.CharField(max_length=120)
-    address = models.CharField(max_length=120)
-    phone = models.CharField(max_length=120)
-    cellphone = models.CharField(max_length=120)
-    email = models.EmailField()
-    birthday = models.DateField()
-    birthplace = models.CharField(max_length=150)
-    authorized_by = models.CharField(max_length=150, null=True)
-    date = models.DateField()
-    accreditation_type = models.CharField(max_length=120, choices=AccreditationType.choices)
-    # Medical Information
-    blood_type = models.CharField(max_length=150)
-    age = models.PositiveIntegerField(default=18)
-    diseases_under_treatment = models.CharField(max_length=150)
-    medications_in_use = models.CharField(max_length=200)
-    have_allergies = models.BooleanField(default=True)
-    allergies = models.CharField(max_length=100, choices=Allergies.choices, blank=True)
-    has_inmunizations = models.BooleanField(default=False)
-    immunizations = models.CharField(max_length=120, choices=Inmunizations.choices, blank=True)
-    have_medical_history = models.BooleanField(default=False)
-    medical_histories = models.CharField(max_length=150, choices=MEDICAL_HISTORIES.choices, blank=True)
-    surgical_history = models.CharField(max_length=150, blank=True)
-    has_personal_doctor = models.BooleanField(default=False)
-    doctor_name = models.CharField(max_length=100, blank=True)
-    # accomodation information
-    hotel_name = models.CharField(max_length=120)
-    hotel_address = models.CharField(max_length=120)
-    hotel_phone = models.CharField(max_length=120)
-    # Flight Information
-    flight_arrival_date = models.DateField()
-    flight_arrival_time = models.TimeField()
-    flight_arrival_number = models.CharField(max_length=120)
-    fligth_procedence = models.CharField(max_length=120)
-    flight_departure_date = models.DateField()
-    flight_departure_time = models.TimeField()
-    flight_departure_number = models.CharField(max_length=120)
-    flight_destination = models.CharField(max_length=120)
-
 
 
 class SecurityAccreditation(models.Model):
