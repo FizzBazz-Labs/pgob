d7e582eabfb3a4c6cac932f7abf7bb4aa502694f
Allan Rodriguez <0x.allan@gmail.com>
2024-02-14 11:35:26 -0600

    feat: clean up overflight form

--- core/admin/overflight_non_commercial_aircraft_admin.py
+++ core/admin/overflight_non_commercial_aircraft_admin.py
@@ -1,55 +1,9 @@
 from django.contrib import admin
-from django.db import models
-from django import forms
 from django.utils.translation import gettext as _
 
-from core.models import *
+from core.models import OverflightNonCommercialAircraft
 
 
 @admin.register(OverflightNonCommercialAircraft)
 class OverflightNonCommercialAircraftAdmin(admin.ModelAdmin):
-
-    fieldsets = [
-        (_('country'), {
-            'fields': [
-                'country'
-            ]
-        }),
-
-        (_('aircraft data'), {
-            'fields': [
-                'aircraft_type',
-                'model',
-                'civilian_military',
-                'registration_number',
-                'color',
-                'call_sign',
-                'commander_name',
-                'crew_members_count',
-                'pmi_name',
-                'position',
-                'passengers_count',
-            ]
-        }),
-
-        (_('flight information'), {
-            'fields': [
-                'entry_date',
-                'exit_date',
-                'overflight_info',
-                'landing_info',
-                'origin',
-                'destination',
-                'route',
-                'ground_facilities',
-
-            ]
-        }),
-
-        (_('signature and dates'), {
-            'fields': [
-                'request_date',
-                'requester_signature',
-            ]
-        })
-    ]
+    ...
--- core/models/overflight_non_commercial_aircraft.py
+++ core/models/overflight_non_commercial_aircraft.py
@@ -1,36 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
+
+
+class Jurisdiction(models.TextChoices):
+    CIVIL = 'civil', _('Civil')
+    MILITARY = 'military', _('Militar')
 
 
 class OverflightNonCommercialAircraft(models.Model):
+    country = models.ForeignKey(
+        'countries.Country',
+        on_delete=models.PROTECT,
+        related_name='flight_forms')
 
-    def upload_file_name(self, filename):
-        return f'fligth_request/{self.accreditation_type}/{filename}'
-
-    def create_image_path(self, filename: str):
-        filename = filename.lower().replace(' ', '').replace('-', '')
-
-        return f'fligth_request/{self.id}/{filename}'
-
-    class CivilianMilitary(models.TextChoices):
-        CIVIL = 'Civil', _('Civil')
-        MILITARY = 'Military', _('Military')
-
-    country = models.ForeignKey('countries.Country', on_delete=models.CASCADE)
-
+    # Aircraft Information
     aircraft_type = models.CharField(max_length=150)
     model = models.CharField(max_length=150)
-    civilian_military = models.CharField(
-        max_length=50, choices=CivilianMilitary.choices)
-    registration_number = models.CharField(max_length=150)
+    jurisdiction = models.CharField(
+        max_length=50,
+        choices=Jurisdiction.choices,
+        default=Jurisdiction.CIVIL)
+    registration = models.CharField(max_length=150)
     color = models.CharField(max_length=150)
     call_sign = models.CharField(max_length=150)
     commander_name = models.CharField(max_length=150)
     crew_members_count = models.IntegerField()
     pmi_name = models.CharField(max_length=150)
-    passengers_count = models.IntegerField()
-
-    # Positions
     position = models.ForeignKey(
         'positions.Position',
         on_delete=models.PROTECT,
@@ -40,10 +36,11 @@ class OverflightNonCommercialAircraft(models.Model):
         on_delete=models.PROTECT,
         blank=True, null=True,
         related_name='flight_forms')
+    passengers_count = models.IntegerField()
 
     # Flight information
     entry_date = models.DateField()
-    exit_date = models.DateField()
+    departure_date = models.DateField()
     overflight_info = models.TextField()
     landing_info = models.TextField()
     origin = models.CharField(max_length=150)
@@ -51,8 +48,13 @@ class OverflightNonCommercialAircraft(models.Model):
     route = models.CharField(max_length=150)
     ground_facilities = models.TextField()
 
-    # signature and dates
-    # PEDIR UNA FIRMA DIGITAL
-    # PREGUNTAR COMO QUIEREN AGARRAR LA
-    request_date = models.DateField()
-    requester_signature = models.CharField(max_length=150)
+    # TODO: Signature File
+    date = models.DateField()
+    signature = models.CharField(max_length=150)
+
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        related_name='flight_forms')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
