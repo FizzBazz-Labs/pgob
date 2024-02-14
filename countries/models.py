b3e84f0833ad5c84325917e32dd702dc0f70e9a9
Allan Rodriguez <0x.allan@gmail.com>
2024-02-13 22:42:05 -0600

    feat: create countries app

--- countries/models.py
+++ countries/models.py
@@ -5,9 +5,9 @@ from django.utils.translation import gettext as _
 class Country(models.Model):
     name = models.CharField(max_length=150)
     nationality = models.CharField(max_length=150)
-    
+
     def __str__(self) -> str:
         return self.name
-    
+
     class Meta:
         verbose_name_plural = _('countries')
