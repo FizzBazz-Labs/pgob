2fb18e7f945aaf7610e6ed7d7342c6ad3aa3ab9c
Allan Rodriguez <0x.allan@gmail.com>
2024-02-13 23:50:53 -0600

    feat: add positions to fixtures

--- positions/admin.py
+++ positions/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from positions.models import Position
+from positions.models import Position, SubPosition
 
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(SubPosition)
+class SubPositionAdmin(admin.ModelAdmin):
     ...
--- positions/models.py
+++ positions/models.py
@@ -4,11 +4,16 @@ from django.utils.translation import gettext_lazy as _
 
 class Position(models.Model):
     name = models.CharField(max_length=250)
+    sub_positions = models.ManyToManyField(
+        'positions.SubPosition',
+        related_name='positions')
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class SubPosition(models.Model):
-    position = models.ForeignKey(
-        'positions.Position',
-        on_delete=models.CASCADE,
-        related_name='sub_positions')
     name = models.CharField(max_length=250)
+
+    def __str__(self) -> str:
+        return self.name
