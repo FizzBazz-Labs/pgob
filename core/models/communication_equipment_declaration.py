ec0f46ac84ac0b75cd350b57312e8eccfe61d926
Allan Rodriguez <0x.allan@gmail.com>
2024-02-15 10:43:19 -0600

    feat: add timestamp to equipments and vechicles declaration

--- core/models/communication_equipment_declaration.py
+++ core/models/communication_equipment_declaration.py
@@ -1,15 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 
 class CommunicationEquipmentDeclaration(models.Model):
-    country = models.ForeignKey('countries.Country', on_delete=models.CASCADE)
+    country = models.ForeignKey(
+        'countries.Country',
+        on_delete=models.PROTECT,
+        related_name='equipment_forms')
     institution_or_media = models.CharField(max_length=150)
+
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        related_name='equipment_forms')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class EquipmentItem(models.Model):
     declaration = models.ForeignKey(
-        CommunicationEquipmentDeclaration,
+        'core.CommunicationEquipmentDeclaration',
         on_delete=models.CASCADE,
         related_name='equipments')
     object_type = models.CharField(max_length=50)
