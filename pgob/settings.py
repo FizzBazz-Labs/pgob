4adc8271c0e88e57c46ebb5ef8e37c8f765aa635
Jason Escobar <jasonescobar20@outlook.com>
2024-02-21 17:19:01 -0600

    feat: add media config

--- pgob/settings.py
+++ pgob/settings.py
@@ -136,6 +136,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'pgob', 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
