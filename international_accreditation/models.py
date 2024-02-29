be97d1b50bfece7512273a6c8773d6fbfd684f42
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2024-02-29 15:58:40 -0600

    adding str to international accreditatino

--- international_accreditation/models.py
+++ international_accreditation/models.py
@@ -127,3 +127,6 @@ class InternationalAccreditation(models.Model):
         related_name='international_forms')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} - {self.last_name} - {self.country.name}'
