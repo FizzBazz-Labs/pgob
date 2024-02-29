5789265b9bacb7202d3ab2717eec13ec3b0c0862
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2024-02-29 15:17:08 -0600

    working general vehicle accreditation'

--- general_vehicle_accreditation/urls.py
+++ general_vehicle_accreditation/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
 
-urlpatterns = []
+from general_vehicle_accreditation.views import GeneralVehicleAccreditationListApiView, GeneralVehicleAccreditationRetrieveApiView
+
+app_name = 'general_vehicle_accreditation'
+
+urlpatterns = [
+    path('general_vehicle_accreditation/',
+         GeneralVehicleAccreditationListApiView.as_view(), name='list-create'),
+    path('general_vehicle_accreditation/<int:pk>/',
+         GeneralVehicleAccreditationRetrieveApiView.as_view(), name='detail'),
+]
--- general_vehicle_accreditation/views.py
+++ general_vehicle_accreditation/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+
+from general_vehicle_accreditation.models import GeneralVehicleAccreditation
+
+from general_vehicle_accreditation.serializers import GeneralVehicleAccreditationSerializer, GeneralVehicleAccreditationReadSerializer
+
+from rest_framework.permissions import IsAuthenticated
+
+class GeneralVehicleAccreditationListApiView(ListCreateAPIView):
+    queryset = GeneralVehicleAccreditation.objects.all()
+    serializer_class = GeneralVehicleAccreditationSerializer
+    permission_classes = [IsAuthenticated]
+
+class GeneralVehicleAccreditationRetrieveApiView(RetrieveAPIView):
+    queryset = GeneralVehicleAccreditation.objects.all()
+    serializer_class = GeneralVehicleAccreditationReadSerializer
