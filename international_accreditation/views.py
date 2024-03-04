662436835981c5f55dcdfb84a198c3ff2aa2feb9
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2024-03-03 18:29:46 -0600

    update to internationsl

--- international_accreditation/views.py
+++ international_accreditation/views.py
@@ -4,7 +4,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 
 from international_accreditation.models import InternationalAccreditation
 
-from international_accreditation.serializers import InternationalAccreditationSerializer, InternationalAccreditationReadSerializer
+from international_accreditation.serializers import InternationalAccreditationSerializer, InternationalAccreditationReadSerializer, InternationalAccreditationUpdateSerializer
 
 from rest_framework.permissions import IsAuthenticated
 
@@ -16,4 +16,8 @@ class InternationalListCreateApiView(ListCreateAPIView):
 
 class InternationalRetrieveApiView(RetrieveUpdateAPIView):
     queryset = InternationalAccreditation.objects.all()
-    serializer_class = InternationalAccreditationReadSerializer
+    def get_serializer_class(self):
+        if self.request.method == 'PUT' or self.request.method == 'PATCH':
+            return InternationalAccreditationUpdateSerializer
+        return InternationalAccreditationReadSerializer    
+    permission_classes = [IsAuthenticated]
