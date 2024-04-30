2f0542ac7512263cdc7ba310d6716fade127fc9c
Allan Rodriguez <0x.allan@gmail.com>
2024-04-29 20:48:31 -0600

    feat: fix pagination to countries

--- countries/urls.py
+++ countries/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 
 from countries.views import CountriesListApiView
 
-app_name = 'countries'
-
 urlpatterns = [
-    path('countries/',
-         CountriesListApiView.as_view(), name='list-create'),
+    path('countries/', CountriesListApiView.as_view()),
 ]
--- countries/views.py
+++ countries/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-
 from rest_framework.generics import ListAPIView
 
 from countries.models import Country
 
 from countries.serializers import CountrySerializer
 
+
 class CountriesListApiView(ListAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    pagination_class = None
