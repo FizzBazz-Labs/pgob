34d3f2d3e68133cc4a872c1188073198f2f7c094
Allan Rodriguez <0x.allan@gmail.com>
2024-05-04 22:27:31 -0600

    feat: add generate accreditation image

--- credentials/urls.py
+++ credentials/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from credentials.views import TestTemplate, TestWeaponAccreditation
-
+from credentials.views import TestTemplate, TestWeaponAccreditation, GenerateAccreditationView
 
 urlpatterns = [
     path('test-template/', TestTemplate.as_view(), name='test-template'),
     path('test-weapon-accreditation/', TestWeaponAccreditation.as_view(),
          name='test-weapon-accreditation'),
+
+    path('pillow/', GenerateAccreditationView.as_view())
 ]
--- credentials/views.py
+++ credentials/views.py
@@ -129,7 +129,8 @@ class GenerateCredential(APIView):
             color = get_accreditation_color(accreditation.type)
             photo = accreditation.image.url
 
-            return generate_pdf_response(photo, color, accreditation_type, accreditation.qr_code.url, accreditation.uuid)
+            return generate_pdf_response(photo, color, accreditation_type, accreditation.qr_code.url,
+                                         accreditation.uuid)
 
         except self.model.DoesNotExist:
             return HttpResponse(status=HTTP_404_NOT_FOUND)
@@ -160,7 +161,6 @@ class TestTemplate(TemplateView):
 
 
 class TestWeaponAccreditation(TemplateView):
-
     template_name = 'credentials/weapon_credential.html'
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
@@ -177,3 +177,34 @@ class TestWeaponAccreditation(TemplateView):
         context['accreditation'] = weapon_accreditation
 
         return context
+
+
+from PIL import Image, ImageDraw
+
+from django.conf import settings
+
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+
+class GenerateAccreditationView(APIView):
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        path = settings.BASE_DIR / 'credentials' / 'static' / 'credentials' / 'images'
+
+        image = Image.open(path / 'credentials.jpeg')
+
+        draw = ImageDraw.Draw(image)
+        draw.text((10, 10), 'Hello World', fill='black')
+
+        image.save(path / 'credentials_edited.jpeg')
+
+        return Response(status=status.HTTP_202_ACCEPTED)
+
+# Save to mission folder
+
+# Add menu action to generate accreditation
+# Show modal and options
+# Generate accreditation
+
+# Add re-accreditate button only works with Accreditator, individual
