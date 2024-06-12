4c7d1ab941f1e80a07b83d9c549d4a20bac3423d
Allan Rodriguez <0x.allan@gmail.com>
2024-06-11 20:53:02 -0600

    feat: add import export general vehicle

--- general_vehicle_accreditation/views.py
+++ general_vehicle_accreditation/views.py
@@ -1,10 +1,16 @@
+from io import BytesIO
+
+import pandas as pd
+from django.db.models import QuerySet
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from core.models import Certification
+from accreditations.mixins import ImportDataMixin
+from core.models import Certification, AccreditationStatus
 from general_vehicle_accreditation.models import GeneralVehicleAccreditation as GeneralVehicle
 from general_vehicle_accreditation.serializers import GeneralVehicleSerializer
 
@@ -13,7 +19,40 @@ from accreditations.views import AccreditationViewSet
 from credentials.utils import certificate_vehicle_accreditation
 
 
-class GeneralVehicleViewSet(AccreditationViewSet):
+def get_data_frame(queryset: QuerySet) -> pd.DataFrame:
+    fields = [
+        'id',
+        'assigned_to',
+        'country__name',
+        'distinctive',
+        'observations',
+        'accreditation_type',
+        'status',
+    ]
+
+    df = pd.DataFrame(queryset.values(*fields))
+
+    df['accreditation_type'] = df['accreditation_type'].apply(
+        lambda x: str(GeneralVehicle.AccreditationType(x).label),
+    )
+    df['status'] = df['status'].apply(
+        lambda x: str(AccreditationStatus(x).label),
+    )
+
+    df = df.rename(columns={
+        'id': 'ID',
+        'assigned_to': 'Asignado A',
+        'country__name': 'País',
+        'distinctive': 'Distintivo',
+        'observations': 'Observaciones',
+        'accreditation_type': 'Tipo de Acreditación',
+        'status': 'Estado',
+    })
+
+    return df
+
+
+class GeneralVehicleViewSet(ImportDataMixin, AccreditationViewSet):
     queryset = GeneralVehicle.objects.all()
     serializer_class = GeneralVehicleSerializer
     filterset_fields = ['status', 'country', 'certificated']
@@ -41,3 +80,22 @@ class GeneralVehicleViewSet(AccreditationViewSet):
                 {"error": "Certification config not found."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+    @action(detail=False, methods=['get'], permission_classes=[AllowAny])
+    def export(self, request, *args, **kwargs):
+        queryset = self.get_queryset().filter(status=AccreditationStatus.PENDING)
+        if not queryset.exists():
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        buffer = BytesIO()
+        get_data_frame(queryset=queryset).to_excel(buffer, index=False)
+        buffer.seek(0)
+
+        response = HttpResponse(
+            buffer.getvalue(),
+            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        )
+
+        response['Content-Disposition'] = 'attachment; filename=data.xlsx'
+
+        return response
