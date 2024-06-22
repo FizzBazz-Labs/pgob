fd9ad20da45cb48328100bdbe671e2574fd7158a
Allan Rodriguez <0x.allan@gmail.com>
2024-06-22 11:52:00 -0600

    migrations

--- vehicles/migrations/0002_vehicle_driver_license.py
+++ vehicles/migrations/0002_vehicle_driver_license.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('vehicles', '0001_initial'),
     ]
--- vehicles/migrations/0003_vehicle_circulation_card.py
+++ vehicles/migrations/0003_vehicle_circulation_card.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('vehicles', '0002_vehicle_driver_license'),
     ]
